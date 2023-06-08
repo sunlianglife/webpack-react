5e304fc99f7b11e04d9bc9389138c145565d8f56
孙亮 <liang.sun@mgcc.onaliyun.com>
2023-06-08 18:29:57 +0800

    feat: init actions

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router } from 'react-router-dom'
 
-const App = () => {
+const App = (a: number) => {
   return (
     <Router>
       <div className="app">
-        <h1>Hello Webpack-Rea ctwww2233</h1>
+        <h1>Hello Webpack-Rea ctwww2233{a?.length}</h1>
       </div>
     </Router>
   )
